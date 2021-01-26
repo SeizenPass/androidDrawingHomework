5d2a437af3a2b46583f63022f15195702f822868
Amiran Kurman <amiranfox@gmail.com>
2021-01-26 12:51:34 +0600

    brush size dialog

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kidsdrawingapp
 
+import android.app.Dialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +8,7 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.ViewFlipper
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,15 +16,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         drawing_view.setSizeForBrush(20.toFloat())
-        findViewById<ImageView>(R.id.ib_small_brush).setOnClickListener {
-            drawing_view.setSizeForBrush(10.toFloat())
-        }
-        findViewById<ImageView>(R.id.ib_medium_brush).setOnClickListener {
-            drawing_view.setSizeForBrush(20.toFloat())
-        }
-        findViewById<ImageView>(R.id.ib_large_brush).setOnClickListener {
-            drawing_view.setSizeForBrush(30.toFloat())
+
+        ib_brush.setOnClickListener{
+            showBrushSizeChooserDialog()
         }
 
+    }
+
+    private fun showBrushSizeChooserDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+        val smallBtn = brushDialog.ib_small_brush
+        smallBtn.setOnClickListener {
+            drawing_view.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+        val mediumBtn = brushDialog.ib_medium_brush
+        mediumBtn.setOnClickListener {
+            drawing_view.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+        val largeBtn = brushDialog.ib_large_brush
+        largeBtn.setOnClickListener {
+            drawing_view.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+        brushDialog.show()
     }
 }
